14c11f04daa3a6be768aa86cf19d0484271a4c3f
Richard Wu <wu.richard.98@gmail.com>
2017-07-03 16:04:33 -0400

    Tabs adjustment

--- hangman.py
+++ hangman.py
@@ -1,6 +1,6 @@
 class Hangman:
-    guy = "  _______\n |/      |\n |      (_)\n |      \|/\n |       |\n |      / \\\n |\n_|___"
+	guy = "  _______\n |/      |\n |      (_)\n |      \|/\n |       |\n |      / \\\n |\n_|___"
 	bad_guess = [] #list of wrongly-guessed letters
-    def __init__(self):
-        print(self.guy)
+	def __init__(self):
+  		print(self.guy)
 h = Hangman()
