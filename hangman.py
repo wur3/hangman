751da85fa17af301f6694d05d91bde6450e40caf
Richard Wu <wu.richard.98@gmail.com>
2017-07-04 17:05:26 -0400

    Added strikes

--- hangman.py
+++ hangman.py
@@ -1,12 +1,14 @@
 class Hangman:
     guy = "  _______\n |/      |\n |      (_)\n |      \|/\n |       |\n |      / \\\n |\n_|___"
     bad_guess = [] #list of wrongly-guessed letters
+	strikes = 0 #at 6 strikes, the hangman is completed and the game ends
     def __init__(self, word):
         self.keyword = list(word) #list that stores the word that is being guessed
         self.display = list('_' * len(word)) #list that starts out as the number-of-letters-in-the-word amount of blank lines e.g. 'cat' -> ['_', '_', '_']
     def take_guess(self, guess):
         if guess not in self.keyword:
             self.bad_guess.append(guess)
+			strikes += 1
         else:
             i = 0
             while(i != len(self.keyword)):
