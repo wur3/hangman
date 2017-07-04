ad9530a26e8492d4e7376c08c99311d6e69b3ddc
Richard Wu <wu.richard.98@gmail.com>
2017-07-04 17:18:52 -0400

    Added losing condition

--- hangmanDriver.py
+++ hangmanDriver.py
@@ -5,9 +5,15 @@ turn = 1
 keep_going = True
 while(keep_going):
     print("\nTurn " + str(turn) + "--------------------------------------")
-    user_guess = input("Guess a letter:\n")
+    user_guess = input("Guess a letter: ")
+    while(user_guess in h.bad_guess or user_guess in h.good_guess):#no repeats
+        user_guess = input("Guess a different letter: ")
     h.take_guess(user_guess)
     print(h.guy + "\n")
+    if(h.strikes >= 6):
+        print("GAME OVER! He got hung.")
+        keep_going = False
+        break
     print(" ".join(h.display) + "\n")
     print("Incorrect: " + ", ".join(h.bad_guess) + "\n")
     if(h.display == h.keyword):
