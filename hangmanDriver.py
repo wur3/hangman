59913fcaf651b4b199a818759c18376ccaf28c97
Richard Wu <wu.richard.98@gmail.com>
2017-10-23 22:42:29 -0400

    Corrected hangman display on turn 1

--- hangmanDriver.py
+++ hangmanDriver.py
@@ -5,21 +5,21 @@ the_word = input_file.read()
 input_file.close()
 h = Hangman(the_word)
 turn = 1
-keep_going = True
-while(keep_going):
+while(True):
     print("\nTurn " + str(turn) + "--------------------------------------")
+    h.print_guy()
+    if(h.strikes >= 6):
+        print(" ".join(h.keyword) + "\nGAME OVER! He got hung.")
+        break
+    print(" ".join(h.display) + "\n")
+    if(h.display == h.keyword):
+        print("Congratulations! You win!")
+        break
+
+    print("Incorrect: " + ", ".join(h.bad_guess) + "\n")
     user_guess = input("Guess a letter: ")
     while(user_guess in h.bad_guess or user_guess in h.good_guess):#no repeats
         user_guess = input("Guess a different letter: ")
     h.take_guess(user_guess)
-    h.print_guy()
-    if(h.strikes >= 6):
-        print("GAME OVER! He got hung.")
-        keep_going = False
-        break
-    print(" ".join(h.display) + "\n")
-    print("Incorrect: " + ", ".join(h.bad_guess) + "\n")
-    if(h.display == h.keyword):
-        print("Congratulations! You win!")
-        keep_going = False
+    
     turn += 1
