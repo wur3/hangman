a6276b22a451ad0d4f8036e59fa8c6e70fc50d61
Richard Wu <wu.richard.98@gmail.com>
2017-11-22 17:04:22 -0500

    Now asks user before starting next game

--- hangmanDriver.py
+++ hangmanDriver.py
@@ -1,19 +1,18 @@
 from hangman import Hangman
 
 input_file = open('./input.txt')
-round = 1
 for line in input_file:
     the_word = line.strip()
     h = Hangman(the_word)
     turn = 1
-    while(True):
+    while (True):
         print("Turn " + str(turn) + "--------------------------------------")
         h.print_guy()
-        if(h.strikes >= 6):
+        if (h.strikes >= 6):
             print(" ".join(h.keyword) + "\nGAME OVER! He got hung.")
             break
         print(" ".join(h.display) + "\n")
-        if(h.display == h.keyword):
+        if (h.display == h.keyword):
             print("Congratulations! You win!")
             h.bad_guess.clear()
             h.good_guess.clear()
@@ -22,9 +21,15 @@ for line in input_file:
 
         print("Incorrect: " + ", ".join(h.bad_guess) + "\n")
         user_guess = input("Guess a letter: ")
-        while(user_guess in h.bad_guess or user_guess in h.good_guess):#no repeats
+        while (user_guess in h.bad_guess or user_guess in h.good_guess):#no repeats
             user_guess = input("Guess a different letter: ")
         h.take_guess(user_guess)
-        
         turn += 1
+    nextGame = input("Want to play another round?(y/n)")
+    while (nextGame.lower() != "yes" and nextGame.lower() != "no" and nextGame.lower() != "y" and nextGame.lower() != "n"):
+        nextGame = input("Yes or no?(y/n):")
+    if (nextGame.lower() == "yes" or nextGame.lower() == "y"):
+        continue
+    elif (nextGame.lower() == "no" or nextGame.lower() == "n"):
+        break
 input_file.close()
