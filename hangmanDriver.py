0dabb0777349b05b6510d444802baefcdde2b857
Richard Wu <wu.richard.98@gmail.com>
2017-11-21 20:17:41 -0500

    Now plays consecutive games if multiple words in input.txt

--- hangmanDriver.py
+++ hangmanDriver.py
@@ -1,25 +1,30 @@
 from hangman import Hangman
 
 input_file = open('./input.txt')
-the_word = input_file.readline().strip()
-input_file.close()
-h = Hangman(the_word)
-turn = 1
-while(True):
-    print("\nTurn " + str(turn) + "--------------------------------------")
-    h.print_guy()
-    if(h.strikes >= 6):
-        print(" ".join(h.keyword) + "\nGAME OVER! He got hung.")
-        break
-    print(" ".join(h.display) + "\n")
-    if(h.display == h.keyword):
-        print("Congratulations! You win!")
-        break
+round = 1
+for line in input_file:
+    the_word = line.strip()
+    h = Hangman(the_word)
+    turn = 1
+    while(True):
+        print("Turn " + str(turn) + "--------------------------------------")
+        h.print_guy()
+        if(h.strikes >= 6):
+            print(" ".join(h.keyword) + "\nGAME OVER! He got hung.")
+            break
+        print(" ".join(h.display) + "\n")
+        if(h.display == h.keyword):
+            print("Congratulations! You win!")
+            h.bad_guess.clear()
+            h.good_guess.clear()
+            h.strikes=0
+            break
 
-    print("Incorrect: " + ", ".join(h.bad_guess) + "\n")
-    user_guess = input("Guess a letter: ")
-    while(user_guess in h.bad_guess or user_guess in h.good_guess):#no repeats
-        user_guess = input("Guess a different letter: ")
-    h.take_guess(user_guess)
-    
-    turn += 1
+        print("Incorrect: " + ", ".join(h.bad_guess) + "\n")
+        user_guess = input("Guess a letter: ")
+        while(user_guess in h.bad_guess or user_guess in h.good_guess):#no repeats
+            user_guess = input("Guess a different letter: ")
+        h.take_guess(user_guess)
+        
+        turn += 1
+input_file.close()
