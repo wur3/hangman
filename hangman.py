dac19aa48f95e421136aa4feffe32fa5b631b347
Richard Wu <wu.richard.98@gmail.com>
2017-11-28 15:18:06 -0500

    Cleaned up syntax with PyCharm

--- hangman.py
+++ hangman.py
@@ -1,24 +1,27 @@
 class Hangman:
-    guy = []
-    guy.append("  _______\n |/      |\n |         \n |      \n |       \n |      \n |\n_|___") #empty
-    guy.append("  _______\n |/      |\n |      (_)\n |         \n |        \n |          \n |\n_|___") #head
-    guy.append("  _______\n |/      |\n |      (_)\n |       | \n |       |\n |          \n |\n_|___") #body
-    guy.append("  _______\n |/      |\n |      (_)\n |      \\| \n |       |\n |          \n |\n_|___") #left-arm
-    guy.append("  _______\n |/      |\n |      (_)\n |      \\|/\n |       |\n |          \n |\n_|___") #right-arm
-    guy.append("  _______\n |/      |\n |      (_)\n |      \\|/\n |       |\n |      /   \n |\n_|___") #left-leg
-    guy.append("  _______\n |/      |\n |      (_)\n |      \\|/\n |       |\n |      / \\\n |\n_|___") #left-leg
-    guy.append("  _______\n |/      |\n |      (_)\n |      \\|/\n |       |\n |      / \\\n |\n_|___") #full
-    bad_guess = [] #list of wrongly-guessed letters
-    good_guess = [] #sublist of keyword, used to check for repeats
-    strikes = 0 #at 6 strikes, the hangman is completed and the game ends
+    guy = [
+        "  _______\n |/      |\n |         \n |      \n |       \n |      \n |\n_|___",  # empty
+        "  _______\n |/      |\n |      (_)\n |         \n |        \n |          \n |\n_|___",  # head
+        "  _______\n |/      |\n |      (_)\n |       | \n |       |\n |          \n |\n_|___",  # body
+        "  _______\n |/      |\n |      (_)\n |      \\| \n |       |\n |          \n |\n_|___",  # left-arm
+        "  _______\n |/      |\n |      (_)\n |      \\|/\n |       |\n |          \n |\n_|___",  # right-arm
+        "  _______\n |/      |\n |      (_)\n |      \\|/\n |       |\n |      /   \n |\n_|___",  # left-leg
+        "  _______\n |/      |\n |      (_)\n |      \\|/\n |       |\n |      / \\\n |\n_|___",  # left-leg
+        "  _______\n |/      |\n |      (_)\n |      \\|/\n |       |\n |      / \\\n |\n_|___"  # full
+    ]
+    bad_guess = []  # list of wrongly-guessed letters
+    good_guess = []  # sublist of keyword, used to check for repeats
+    strikes = 0  # at 6 strikes, the hangman is completed and the game ends
+
     def __init__(self, word):
-        self.keyword = list(word) #list that stores the word that is being guessed
-        self.display = [];
-        for i in range(0,len(word)):
-            if(self.keyword[i]!=' '):
-                self.display.append('_');
+        self.keyword = list(word)  # list that stores the word that is being guessed
+        self.display = []
+        for i in range(0, len(word)):
+            if self.keyword[i] != ' ':
+                self.display.append('_')
             else:
-                self.display.append(' ');
+                self.display.append(' ')
+
     def take_guess(self, guess):
         if guess not in self.keyword:
             self.bad_guess.append(guess)
@@ -26,14 +29,17 @@ class Hangman:
         else:
             self.good_guess.append(guess)
             i = 0
-            while(i != len(self.keyword)):
+            while i != len(self.keyword):
                 try:
-                    x = self.keyword[i:].index(guess) #index value where 'guess' is found in 'keyword'
+                    x = self.keyword[i:].index(guess)  # index value where 'guess' is found in 'keyword'
                 except ValueError:
                     break
-                self.display[x+(len(self.keyword)-len(self.keyword[i:]))] = guess #compensates for shift in indices values for keyword versus keyword[i:]
-                i+=1
-    def print_guy(self): #print hangman guy
+                # compensates for shift in indices values for keyword versus keyword[i:]
+                self.display[x+(len(self.keyword)-len(self.keyword[i:]))] = guess
+                i += 1
+
+    def print_guy(self):  # print hangman guy
         print(self.guy[self.strikes])
-    def print_bad_guess(self): #print list of wrong guesses
+
+    def print_bad_guess(self):  # print list of wrong guesses
         print(self.bad_guess)
--- hangmanDriver.py
+++ hangmanDriver.py
@@ -5,31 +5,32 @@ for line in input_file:
     the_word = line.strip()
     h = Hangman(the_word)
     turn = 1
-    while (True):
+    while True:
         print("Turn " + str(turn) + "--------------------------------------")
         h.print_guy()
-        if (h.strikes >= 6):
+        if h.strikes >= 6:
             print(" ".join(h.keyword) + "\nGAME OVER! He got hung.")
             break
         print(" ".join(h.display) + "\n")
-        if (h.display == h.keyword):
+        if h.display == h.keyword:
             print("Congratulations! You win!")
             h.bad_guess.clear()
             h.good_guess.clear()
-            h.strikes=0
+            h.strikes = 0
             break
 
         print("Incorrect: " + ", ".join(h.bad_guess) + "\n")
         user_guess = input("Guess a letter: ")
-        while (user_guess in h.bad_guess or user_guess in h.good_guess):#no repeats
+        while user_guess in h.bad_guess or user_guess in h.good_guess:  # no repeats
             user_guess = input("Guess a different letter: ")
         h.take_guess(user_guess)
         turn += 1
     nextGame = input("Want to play another round?(y/n)")
-    while (nextGame.lower() != "yes" and nextGame.lower() != "no" and nextGame.lower() != "y" and nextGame.lower() != "n"):
+    while nextGame.lower() != "yes" and nextGame.lower() != "no" \
+            and nextGame.lower() != "y" and nextGame.lower() != "n":
         nextGame = input("Yes or no?(y/n):")
-    if (nextGame.lower() == "yes" or nextGame.lower() == "y"):
+    if nextGame.lower() == "yes" or nextGame.lower() == "y":
         continue
-    elif (nextGame.lower() == "no" or nextGame.lower() == "n"):
+    elif nextGame.lower() == "no" or nextGame.lower() == "n":
         break
 input_file.close()
